1da6d48ffeb75e24a6565e80b104593e67dd82f6
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-01-22 12:35:25 +0100

    If there were old values saved that no longer appear in the options, we display them as selected. If they are deselected we remove them from the options.

--- src/js/Components/Setting/Settings/CheckboxMultiple.jsx
+++ src/js/Components/Setting/Settings/CheckboxMultiple.jsx
@@ -1,10 +1,59 @@
-import l from "../../../utils";
+import l, { store_slug, prepareValue } from "../../../utils";
 
-const { without, isEmpty } = lodash;
+const { without, isEmpty, forEach } = lodash;
 const { Component } = wp.element;
+const { withSelect } = wp.data;
 const { BaseControl, CheckboxControl, ToggleControl } = wp.components;
 
 class CheckboxMultiple extends Component {
+	getOptions = () => {
+		const { getOldOptions, getDefaultOptions, props } = this;
+		const { options } = props;
+		const old_options = getOldOptions();
+		const default_options = getDefaultOptions(old_options);
+
+		return options.concat(old_options, default_options);
+	};
+
+	getDefaultOptions = old_options => {
+		const { default_value, options } = this.props;
+		const default_options = [];
+		const options_key = options.map(({ value }) => value);
+		const old_options_key = old_options.map(({ value }) => value);
+
+		forEach(default_value, value => {
+			if (
+				!options_key.includes(value) &&
+				!old_options_key.includes(value) &&
+				value !== ""
+			) {
+				default_options.push({ value: value, label: value });
+			}
+		});
+
+		return default_options;
+	};
+
+	// If there were old selected values that are no longer included
+	// in the options array, we still display them.
+	// But when they are deselected and the post is saved they will no longer appear.
+	getOldOptions = () => {
+		const { value_initial, options } = this.props;
+		const old_options = [];
+		const options_key = options.map(({ value }) => value);
+
+		forEach(value_initial, value => {
+			if (!options_key.includes(value)) {
+				old_options.push({
+					value: value,
+					label: value
+				});
+			}
+		});
+
+		return old_options;
+	};
+
 	onChangeHandler = (selected, option_value) => {
 		const { value, updateValue } = this.props;
 		let value_new;
@@ -28,8 +77,9 @@ class CheckboxMultiple extends Component {
 	};
 
 	render() {
-		const { onChangeHandler, props } = this;
-		const { label, help, value, options, use_toggle } = props;
+		const { onChangeHandler, getOptions, props } = this;
+		const { label, help, value, use_toggle } = props;
+		const options = getOptions();
 
 		return (
 			<BaseControl label={label} help={help}>
@@ -63,4 +113,17 @@ class CheckboxMultiple extends Component {
 	}
 }
 
-export default CheckboxMultiple;
+export default withSelect((select, { setting_id }) => {
+	const { getCurrentPostAttribute } = select("core/editor");
+	const { getSetting, getPersistedProp } = select(store_slug);
+	const setting = getSetting(setting_id);
+	const value_initial = prepareValue(
+		setting,
+		getPersistedProp,
+		getCurrentPostAttribute
+	);
+
+	return {
+		value_initial
+	};
+})(CheckboxMultiple);
