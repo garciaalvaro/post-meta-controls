33bd44e3fb7fcbe17a85b6f3ca31ad8d9cdad286
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-02-12 16:02:42 +0900

    Added eslint and prettier packages and config files

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier/@typescript-eslint",
+		"plugin:prettier/recommended"
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	rules: {
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/camelcase": "off",
+		"@typescript-eslint/no-var-requires": "off",
+		"@typescript-eslint/ban-ts-ignore": "off",
+		"react/react-in-jsx-scope": "off",
+		"react/prop-types": "off"
+	},
+	settings: {
+		react: {
+			version: "detect"
+		},
+		"import/resolver": {
+			alias: {
+				map: [
+					["utils", "src/utils"],
+					["store", "src/store"],
+					["Components", "src/Components"]
+				]
+			}
+		}
+	}
+};
